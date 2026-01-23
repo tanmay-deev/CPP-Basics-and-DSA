3ec15ee45ec4e84667ddd7a0e1dd69d5cf06ac0f
Tanmay <tanmaybonde10@gmail.com>
2026-01-23 11:08:46 +0530

    patterns

---
+++ Pattern/pattern15.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    //      *
+    //     * *
+    //    * * *
+    //     * *
+    //      *
+
+    // Print Diamond Pattern
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n - i; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= i; k++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i = 1; i <= n; i--){
+        for(int j = 1; j <= n - i; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= i; k--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+
+}
